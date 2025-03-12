f9a620b52fed686f0ad6871bd11e5e5bd599db9c
AubreyMartin <aubreymartin9000@gmail.com>
2025-03-12 16:19:51 +0530

    Removed unused state variables to fix TS6133 errors in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 import Countdown from "./components/Countdown";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Countdown />
